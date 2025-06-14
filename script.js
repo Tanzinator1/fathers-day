8a6242485ec05dc89f06ded8e17082e637b7d914
Ryan <tanzerry@msu.edu>
2025-06-14 14:25:55 -0400

    Update file extensions to .JPG and .JPEG

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let currentIndex = 0;
 const slides = document.querySelectorAll('.slide');
 const slider = document.getElementById('slider');
 
+// Dynamically set the total width of the slider based on number of slides
+slider.style.width = `${slides.length * 100}%`;
+
 function showSlide(index) {
   const offset = index * -100;
   slider.style.transform = `translateX(${offset}%)`;
